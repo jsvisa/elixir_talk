d7ce75598f277107caf7da54cec9e6eb2d8fa791
jsvisa <delweng@gmail.com>
2014-03-01 18:54:41 +0800

    update: add test case

--- test/elixir_talk_test.exs
+++ test/elixir_talk_test.exs
@@ -1,7 +1,15 @@
 defmodule ElixirTalkTest do
   use ExUnit.Case
 
-  test "the truth" do
-    assert(true)
+  setup_all do
+    IO.puts "connect to beanstalkd first"
+    {:ok, _} = ElixirTalk.connect "10.1.1.5"
+    :ok
+  end
+
+
+  test "`put` " do
+    {:inserted, id} = ElixirTalk.put "hello world"
+    assert is_integer(id) == true
   end
 end
