b7f5475997bd9d9aec567aca4a5a64c9193890e9
jsvisa <delweng@gmail.com>
2014-07-21 19:23:50 +0800

    Delete Supervisor module

--- mix.exs
+++ mix.exs
@@ -10,7 +10,7 @@ defmodule Beanstalk.Mixfile do
 
   # Configuration for the OTP application
   def application do
-    [registered: [Beanstalk.Supervisor, Beanstalk.Connect]]
+    []
   end
 
   # Returns the list of dependencies in the format:
