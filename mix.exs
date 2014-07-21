6d75cdbfb42c428bd170ed0af62ca1192830927d
jsvisa <delweng@gmail.com>
2014-07-21 18:38:11 +0800

    Update Elixir to 0.14.3

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Beanstalk.Mixfile do
   def project do
     [ app: :beanstalk,
       version: "0.0.1",
-      elixir: "~> 0.12.4-dev",
+      elixir: "~> 0.14.3",
       deps: deps ]
   end
 
